7402d5e7f49f49adaa0cb953efdc3a2c979dd067
David Hughes <d@vidhughes.com>
2012-07-13 16:25:28 -0700

    Adds unzip as a dependency

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh
+sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh unzip
 sudo apt-get --yes install exuberant-ctags libjline-java x11-xserver-utils
 sudo apt-get --yes install python-pip
 
