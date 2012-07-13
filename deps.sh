56beeda6fced1e51ee246f427263e9a59c13078c
David Hughes <d@vidhughes.com>
2012-07-13 14:59:45 -0700

    Added rubygems to deps and added flag for non-interactive install

--- deps.sh
+++ deps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo apt-get install vim-nox tmux zsh ruby ruby-dev ssh exuberant-ctags libjline-java x11-xserver-utils
+sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh exuberant-ctags libjline-java x11-xserver-utils
 
 sudo gem install tmuxinator
