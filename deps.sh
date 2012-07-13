3156a4576443ee859a6c03854584bae4d099eb5a
David Hughes <d@vidhughes.com>
2012-07-13 15:04:41 -0700

    Added python-pip to dependencies

--- deps.sh
+++ deps.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh exuberant-ctags libjline-java x11-xserver-utils
+sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh
+sudo apt-get --yes install exuberant-ctags libjline-java x11-xserver-utils
+sudo apt-get --yes install python-pip
 
 sudo gem install tmuxinator
