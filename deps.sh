082ab028313eae0d504749321dbb70412cf9f505
David Hughes <d@vidhughes.com>
2012-09-24 10:25:33 -0700

    Added xdotool as dependency

--- deps.sh
+++ deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get --yes install vim-nox tmux zsh ruby ruby-dev rubygems ssh unzip
-sudo apt-get --yes install exuberant-ctags libjline-java x11-xserver-utils
+sudo apt-get --yes install exuberant-ctags libjline-java
 sudo apt-get --yes install python-pip
+sudo apt-get --yes install x11-xserver-utils xdotool
 
 sudo gem install tmuxinator
