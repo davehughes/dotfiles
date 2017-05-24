81f8361f5bc4331e2afc1aebea8f8d5ae01ccb8f
David Hughes <d@vidhughes.com>
2017-05-24 14:46:22 -0700

    fasd ls aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -8,6 +8,8 @@ alias v='fasd -fe vim'
 alias vv='fasd -fise vim'
 alias j='fasd_cd -d'
 alias jj='fasd_cd -d -i'
+alias l='fasd -de ls'
+alias ll='fasd -dise ls'
 
 alias vim="nvim"
 alias oldvim="vim"
